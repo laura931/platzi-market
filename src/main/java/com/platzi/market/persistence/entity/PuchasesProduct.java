079dbdee9e6c626ffaeef60beec2fdd4dcfe7d0f
laura931 <lauramazawd@gmail.com>
2023-12-02 16:00:19 -0300

    add EmbeddedId (clave primaria compuesta)

--- src/main/java/com/platzi/market/persistence/entity/PuchasesProduct.java
+++ src/main/java/com/platzi/market/persistence/entity/PuchasesProduct.java
@@ -1,5 +1,6 @@
 package com.platzi.market.persistence.entity;
 
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
@@ -11,6 +12,42 @@ import jakarta.persistence.Table;
 @Table(name="compras-productos")
 public class PuchasesProduct {
 
+    @EmbeddedId
+    private PurchasesProductPK id;
 
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
 
+    public PurchasesProductPK getId() {
+        return id;
+    }
+
+    public void setId(PurchasesProductPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
--- src/main/java/com/platzi/market/persistence/entity/PurchasesProductPK.java
+++ src/main/java/com/platzi/market/persistence/entity/PurchasesProductPK.java
@@ -1,4 +1,33 @@
 package com.platzi.market.persistence.entity;
 
-public class PurchasesProductPK {
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+
+@Embeddable
+public class PurchasesProductPK implements Serializable {
+
+    @Column(name="id_compra")
+    private Integer idCompra;
+
+    @Column(name="id_producto")
+    private Integer idProducto;
+
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
 }
