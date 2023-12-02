9aaae09a6f31ccb2ac6180bf95ef1b5ad370a85d
laura931 <lauramazawd@gmail.com>
2023-12-02 20:50:24 -0300

    fix metodos de ejemplo

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -2,15 +2,16 @@ package com.platzi.market.persistence.crud;
 
 import com.platzi.market.persistence.entity.Categoria;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
     //Ejemplo de query native
-    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", queryNative = true)
-    List<Categoria> getById(int idCategoria);
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getById(int idCategoria);
 
     //usando queryMethods
-    List<Categoria> findByIdCategoria(int idCategoria);
+    List<Producto> findByIdCategoria(int idCategoria);
 }
