a84a38265b836e9e70b616fd8896b1018e7b52e6
laura931 <lauramazawd@gmail.com>
2023-12-02 20:44:43 -0300

    add ej query native and query methods

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,16 @@
 package com.platzi.market.persistence.crud;
 
+import com.platzi.market.persistence.entity.Categoria;
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //Ejemplo de query native
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", queryNative = true)
+    List<Categoria> getById(int idCategoria);
+
+    //usando queryMethods
+    List<Categoria> findByIdCategoria(int idCategoria);
 }
