7a0ea4f37a32aaf39f3e5dc93ced5b3ceaca4207
laura931 <lauramazawd@gmail.com>
2023-12-02 21:08:15 -0300

    add Ejemplos de uso de queryMethod con Optional

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -4,11 +4,20 @@ import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreASC(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCategoriaStockLessthanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -6,12 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
     //Ejemplo de query native
-    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
-    List<Producto> getById(int idCategoria);
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    //List<Producto> getById(int idCategoria);
 
     //usando queryMethods
-    List<Producto> findByIdCategoria(int idCategoria);
+    public List<Producto> findByIdCategoriaOrderByNombreASC(int idCategoria);
+
+    public Optional<List<Producto>> findByCategoriaStockLessthanAndEstado(int idCategoria, boolean estado);
 }
