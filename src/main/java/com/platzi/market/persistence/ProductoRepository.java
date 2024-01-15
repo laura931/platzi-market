212a8cabd0e9cd6946ba0ce2912ca6fe1a078d08
laura931 <lauramazawd@gmail.com>
2024-01-15 18:21:53 -0300

    renamed ProductoRepository de la capa de persistencia y add commentarios

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,17 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * El MapStruct de los productos para usar en la capa de la Api es Product
+ * Los métodos que se implementarán para comunicarse con el MapStruct están definidos en la interface ProductInterfase
+ * De este modo, siempre hablaremos en términos del dominio (Product) y no de la capa de persistencia (Producto)
+ */
+@Repository
 public class ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +26,17 @@ public class ProductRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCategoriaStockLessthanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
